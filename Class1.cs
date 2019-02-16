74ed33e3b4aabacb828d523aec43470a1c134836
dimitrismatsanganis <34712449+dimitrismatsanganis@users.noreply.github.com>
2019-02-16 13:39:47 +0200

    Add first class, Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Class1
+    {
+        public double answer;
+        public double count;
+        public double num;
+    }
+}
